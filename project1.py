2db42968dad577f6974705a7f967837b5a9e626c
sufuf3 <sufuf3@gmail.com>
2016-11-07 21:54:56 +0800

    multiplication table using while loop

--- project1.py
+++ project1.py
@@ -1,8 +1,23 @@
 # -*- coding:utf-8 -*-
 
-print "Hello World!"
+print "1. Write a program to print “Hello World”\n"
+print "Hello World!\n"
 
+print "==============================\n"
+print "2. Modify the program to print 9x9 multiplication table using for loop\n"
 for i in range(1,10):
     for j in range(1,10):
         print i * j,
+    print "\n"
+
+print "==============================\n"
+print "3. Use while loop instead of for loop to print 9x9 multiplication table\n"
+x = 1
+y = 1
+while x <= 9:
+    while y <= 9:
+        print x * y,
+        y=y+1
+    x=x+1
+    y=1
     print "\n"
